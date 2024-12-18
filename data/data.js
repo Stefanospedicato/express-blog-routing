78329eeaa9b1eee0582ceb6fb5202a61a8c2b9b2
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-12-18 12:03:04 +0100

    docs: add readme file feat: add data.js add imgs in public

---
+++ data/data.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    titolo:'ciambellone',
+    contenuto:'ciambella',
+    img:'.img/ciambellone.jpeg',
+    tags:['cimbellone','zucchero']
+  },
+  {
+    titolo:'cracker',
+    contenuto:'cracker con la barbabietola',
+    img:'.img/cracker_barbabietola.jpeg',
+    tags:['cracker','barbabietola']
+  },
+  {
+    titolo:'pane',
+    contenuto:'pane fritto dolce',
+    img:'.img/pane_fritto_dolce.jpeg',
+    tags:['school','homework']
+  },
+  {
+    titolo:'pasta',
+    contenuto:'pasta alla barbabietola',
+    img:'.img/pasta_barbabietola.jpeg',
+    tags:['pasta','barbabietola']
+  },
+  {
+    titolo:'torta',
+    contenuto:'torta paesana',
+    img:'.img/torta_paesana.jpeg',
+    tags:['torta','paesana']
+  }
+]
+
+module.exports = posts
